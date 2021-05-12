c8c93566faa0d98e443c31af1af033ea30181fb3
Dmitry Mazurov <dimabzz@gmail.com>
2021-05-12 20:50:37 +0300

    Fixed indent
    Signed-off-by: Dmitry Mazurov <dimabzz@gmail.com>

--- ups.sh
+++ ups.sh
@@ -6,101 +6,101 @@ TEST="$1"
 CACHE_STATUS=''
 
 if ! which apcupsd &> /dev/null; then
-	echo "apcupsd not installed"
-	exit 1
+  echo "apcupsd not installed"
+  exit 1
 fi
 
 if [ ! -f "$CONF" ]; then
-	echo "File configuration not found. Please copy ups.example.conf to ups.conf"
-	exit 1
+  echo "File configuration not found. Please copy ups.example.conf to ups.conf"
+  exit 1
 else
-	# shellcheck disable=SC1090
-	source "${CONF}"
+  # shellcheck disable=SC1090
+  source "${CONF}"
 fi
 
 if [ -z "$TYPE" ] || [ -z "$ID" ] || [ -z "$KEY" ] || [ -z "$FILE" ]; then
-	echo "The config file is configured incorrectly"
-	exit 1
+  echo "The config file is configured incorrectly"
+  exit 1
 fi
 
 if [ "$TYPE" = "multicast" ] && [ -z "$UIDS" ]; then
-	echo "You are using multicast request type. UIDS must be set"
-	exit 1
+  echo "You are using multicast request type. UIDS must be set"
+  exit 1
 fi
 
 if [ "$TYPE" = "unicast" ] && [ -z "$UIDD" ]; then
-				echo "You are using unicast request type. UIDD must be set"
-				exit 1
+  echo "You are using unicast request type. UIDD must be set"
+  exit 1
 fi
 
 if [ -n "$TEST" ]; then
-	echo -e "Running test"
+  echo -e "Running test"
 fi
 
 push() {
-	local -r status="$1"
-	local -r bcharge="$2"
-	local -r upsname="${3:-UPS}"
+  local -r status="$1"
+  local -r bcharge="$2"
+  local -r upsname="${3:-UPS}"
 
-	# shellcheck disable=SC2153
-	RTITLE="${TITLE//#upsname/$upsname}"
-	# shellcheck disable=SC2153
-	RTEXT=$(echo "${TEXT}" | sed "s/#status/${status}/; s/#bcharge/${bcharge}/; s/#upsname/${upsname}/;")
+  # shellcheck disable=SC2153
+  RTITLE="${TITLE//#upsname/$upsname}"
+  # shellcheck disable=SC2153
+  RTEXT=$(echo "${TEXT}" | sed "s/#status/${status}/; s/#bcharge/${bcharge}/; s/#upsname/${upsname}/;")
 
-	if [ -z "$RTITLE" ]; then
-		RTITLE="Status ${upsname}"
-	fi
+  if [ -z "$RTITLE" ]; then
+    RTITLE="Status ${upsname}"
+  fi
 
-	if [ -z "$RTEXT" ]; then
-		RTEXT="Status UPS: ${status} Battery Charge: ${bcharge}%"
-	fi
+  if [ -z "$RTEXT" ]; then
+    RTEXT="Status UPS: ${status} Battery Charge: ${bcharge}%"
+  fi
 
-	curl -sd "type=${TYPE}&id=${ID}&key=${KEY}&ttl=${TTL}&uid=${UIDD}&uids=${UIDS}&title=${RTITLE}&text=${RTEXT}" https://pushall.ru/api.php
+  curl -sd "type=${TYPE}&id=${ID}&key=${KEY}&ttl=${TTL}&uid=${UIDD}&uids=${UIDS}&title=${RTITLE}&text=${RTEXT}" https://pushall.ru/api.php
 }
 
 check() {
-	if [ -z "$TEST" ] && [ ! -f "$FILE" ]; then
-		echo "File ${FILE} not found"
-		exit 1
-	fi
+  if [ -z "$TEST" ] && [ ! -f "$FILE" ]; then
+    echo "File ${FILE} not found"
+    exit 1
+  fi
 
-	if [ -z "$TEST" ]; then
-		# shellcheck disable=SC2002
-		UPSNAME=$(cat "${FILE}" | awk '/^(UPSNAME).*:/ {$1=$2="";print $0}' | sed -e 's/^[[:space:]]*//')
-		# shellcheck disable=SC2002
-		STATUS=$(cat "${FILE}" | awk '/^(STATUS).*:/ {print $3}')
-		# shellcheck disable=SC2002
-		BCHARGE=$(cat "${FILE}" | awk '/^(BCHARGE).*:/ {print $3}')
-	else
-		UPSNAME=""
-		array=("ONLINE" "ONBATT")
-		BCHARGE=$(( (RANDOM % 100 )  + 1 ))
-		size="${#array[@]}"
-		index=$((RANDOM % size))
-		STATUS="${array[$index]}"
-	fi
+  if [ -z "$TEST" ]; then
+    # shellcheck disable=SC2002
+    UPSNAME=$(cat "${FILE}" | awk '/^(UPSNAME).*:/ {$1=$2="";print $0}' | sed -e 's/^[[:space:]]*//')
+    # shellcheck disable=SC2002
+    STATUS=$(cat "${FILE}" | awk '/^(STATUS).*:/ {print $3}')
+    # shellcheck disable=SC2002
+    BCHARGE=$(cat "${FILE}" | awk '/^(BCHARGE).*:/ {print $3}')
+  else
+    UPSNAME=""
+    array=("ONLINE" "ONBATT")
+    BCHARGE=$(( (RANDOM % 100 )  + 1 ))
+    size="${#array[@]}"
+    index=$((RANDOM % size))
+    STATUS="${array[$index]}"
+  fi
 
-	if [ -z "$STATUS" ]; then
-		echo "UPS Status not found"
-		exit 1
-	fi
+  if [ -z "$STATUS" ]; then
+    echo "UPS Status not found"
+    exit 1
+  fi
 
-	if [ "$CACHE_STATUS" != "$STATUS" ]; then
-		if [ -z "$TEST" ]; then
-			CACHE_STATUS="${STATUS}"
-		fi
-		push "${STATUS}" "${BCHARGE}" "${UPSNAME}"
-	fi
+  if [ "$CACHE_STATUS" != "$STATUS" ]; then
+    if [ -z "$TEST" ]; then
+      CACHE_STATUS="${STATUS}"
+    fi
+    push "${STATUS}" "${BCHARGE}" "${UPSNAME}"
+  fi
 }
 
 while :
 do
-	check
+  check
 
-	if [ -n "$TEST" ]; then
-		echo -e
-		exit 0
-	fi
+  if [ -n "$TEST" ]; then
+    echo -e
+    exit 0
+  fi
 
-	sleep "${INTERVAL:-15}"
+  sleep "${INTERVAL:-15}"
 done
